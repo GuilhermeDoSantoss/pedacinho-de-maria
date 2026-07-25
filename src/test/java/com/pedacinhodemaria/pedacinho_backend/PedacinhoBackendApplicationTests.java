71ac415c7c1976b4d95a4680351963cde0bca3fa
Guilherme dos Santos <guilherme.devsoft@gmail.com>
2026-07-25 10:31:14 -0300

    projeto criado

---
+++ src/test/java/com/pedacinhodemaria/pedacinho_backend/PedacinhoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedacinhodemaria.pedacinho_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedacinhoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
